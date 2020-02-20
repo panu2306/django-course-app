965df9d7b412b561759cfc4986aef2cd02cbd70e
panu2306 <pranavbhendawade@gmail.com>
2020-02-20 22:32:48 +0530

    Custom validation for catching bots on forms

--- first_app/forms.py
+++ first_app/forms.py
@@ -4,4 +4,13 @@ class NameForm(forms.Form):
     name = forms.CharField(max_length=64)
     email = forms.EmailField()
     text = forms.CharField(widget=forms.Textarea)
+    botcatcher = forms.CharField(required=False, widget=forms.HiddenInput)
+
+    def clean_botcatcher(self):
+        botcatcher_field = self.cleaned_data['botcatcher']
+        
+        if(len(botcatcher_field) > 0):
+            raise forms.ValidationError("Gotcha Bot")        
+        return botcatcher_field
+
     
